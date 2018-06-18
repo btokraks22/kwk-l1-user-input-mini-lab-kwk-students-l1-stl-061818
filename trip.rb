1be8fa727e1728640388319bed96495856f4ed08
btokraks22 <bree.v.v.13@gmail.com>
2018-06-18 20:15:22 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,6 +1,11 @@
 # Code your prompts here!
 puts "Hello, where would you like to stay?"
-place = 
+place = gets
+puts "I hear #{place} is very nice! What attractions do you want to see?"
+attraction = gets
+puts "#{attraction} is very fun for tourists. What kind of food do you want to eat?"
+food = gets
+puts "#{food} Oh my! that sounds delicious. Finally, how many nights would you like to stay?"
 # First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
 
